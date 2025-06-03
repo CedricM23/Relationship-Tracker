2b9443ed85e68934b82e76ac338f9b4ba5d43e93
cedricm23 <Cedricpbc@gmail.com>
2025-06-03 10:08:16 -0400

    Made a placeholder object

--- src/services/DatesService.js
+++ src/services/DatesService.js
@@ -10,11 +10,7 @@ export default {
     
 }
 
-let placeholderImage = 'https://placehold.co/600x400';
-
-
-const dates = [
-    {
+let placeholderObject = {
         id: 1,
         name: 'Moon Thai & Japanese',
         location: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3564.9448239270328!2d-80.22804948851044!3d26.682247076688412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d92f0f5a2af889%3A0xf6a9ffa6148838ec!2sMoon%20Thai%20%26%20Japanese!5e0!3m2!1sen!2sus!4v1748902194032!5m2!1sen!2sus',
@@ -25,20 +21,11 @@ const dates = [
         Descriptionp1:'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
         Descriptionp2:'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
         Descriptionp3: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
-    },
-    {
-        id: 2,
-        name: 'placeholder',
-        imageOfPlace: placeholderImage,
-    },
-    {
-        id: 3,
-        name: 'placeholder',
-        imageOfPlace: placeholderImage,
-    },
-    {
-        id: 3,
-        name: 'placeholder',
-        imageOfPlace: placeholderImage,
     }
+
+
+const dates = [
+    placeholderObject,
+    placeholderObject,
+    placeholderObject,
 ]
