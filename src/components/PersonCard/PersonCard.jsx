384651d6cbb40ba94b44024fe4f2d8000c6286c6
cedricm23 <Cedricpbc@gmail.com>
2025-06-07 14:36:19 -0400

    fixed image

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -1,4 +1,5 @@
 import styles from './PersonCard.module.css'
+import placholderimage from '../../images/avatar-1577909_640.png'
 
 export default function Personcard({person, imagewidth}) {
 
@@ -9,7 +10,7 @@ export default function Personcard({person, imagewidth}) {
         <section className={styles.personcard}>
             {person.profile_path != null ?
         <img className={styles.image} src={`${imagepath}${person.profile_path}`} alt="" /> : 
-        <img className={styles.placeholderimage} src="src/images/avatar-1577909_640.png" alt="" />}
+        <img className={styles.placeholderimage} src={placholderimage} alt="" />}
         {person.name}
         </section>
         </>
