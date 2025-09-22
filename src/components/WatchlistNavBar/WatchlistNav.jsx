486df1fe052b49981edbe3442d663857e370b768
cedricm23 <Cedricpbc@gmail.com>
2025-09-22 15:34:39 -0400

    changed Favorites to My Lists

--- src/components/WatchlistNavBar/WatchlistNav.jsx
+++ src/components/WatchlistNavBar/WatchlistNav.jsx
@@ -8,7 +8,7 @@ export default function WatchlistNav(){
         <Link to='/shows' className={styles.links}>Tv Shows</Link>
         <Link to='/upcoming' className={styles.links}>Upcoming</Link>
         <Link to='/trending' className={styles.links}>Trending</Link>
-        <Link to='/favorites' className={styles.links}>Favorites</Link>
+        <Link to='/favorites' className={styles.links}>My Lists</Link>
     </nav>
     )
 }
