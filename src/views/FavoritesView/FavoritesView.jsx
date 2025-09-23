2f975219d68b620e9ea541d384add76a98293add
cedricm23 <Cedricpbc@gmail.com>
2025-09-23 16:51:52 -0400

    Commented out unused code for cleanup

--- src/views/FavoritesView/FavoritesView.jsx
+++ src/views/FavoritesView/FavoritesView.jsx
@@ -9,7 +9,7 @@ import ListDetailCard from "../../components/ListDetailCard/ListDetailCard";
 
 export default function FavoritesView() {
     const [lists, setLists] = useState([])
-    const [listdetails, setListDetails] = useState([])
+    // const [listdetails, setListDetails] = useState([])
     let listid;
 
     useEffect(() => {
@@ -19,15 +19,15 @@ export default function FavoritesView() {
                 console.log(response.data)
                 setLists(response.data.results)
 
-                //Fav list hardcoded
-                ShowService.getListDetailsById(8559472)
-                    .then((response) => {
-                        setListDetails(response.data.results)
-                        console.log('')
-                        console.log(response.data)
-                    }).catch(() => {
-                        alert('Could not get your list item! please try again!');
-                    });
+                // //Fav list hardcoded
+                // ShowService.getListDetailsById(8559472)
+                //     .then((response) => {
+                //         setListDetails(response.data.results)
+                //         console.log('')
+                //         console.log(response.data)
+                //     }).catch(() => {
+                //         alert('Could not get your list item! please try again!');
+                //     });
 
 
 
@@ -82,7 +82,6 @@ export default function FavoritesView() {
     return (
         <>
             <WatchlistNavBar />
-            <p style={{ color: 'red', fontSize: '20px' }}>DATA CANNOT BE PULLED FOR MULTIPLE LISTS</p>
             <button onClick={handleClick}>New List</button>
             <button onClick={handleClose}>Close List</button>
             <div className={styles.form} id="form">
@@ -96,25 +95,13 @@ export default function FavoritesView() {
                     {/* move lists into sepereate componenets and render each lists data seperately (this might work) make sure Favorites list is on the top*/}
                     {lists.map(
                         (list) => (
-                            // <div className={styles.list}>
-                            //     <div className={styles.header}>
-                            //         <h1 className={styles.listname}>{list.name}</h1>
-                            //         <div className={styles.middlesection}></div>
-                            //         <button onClick={handleDelete} className={styles.headerbutton} data-id={list.id}>
-                            //             { list.id === 8559472 ? <></> : <FontAwesomeIcon icon={faTrash} className={styles.headericon} />}
-                            //         </button>
-                            //     </div>
-                            //     <div className={styles.listgrid} >
-                             
-                            //         {listdetails.map(
-                            //             (item, index) => (
-                            //                 <div key={index}>{item.original_title ? item.original_title : item.name} {item.id}</div>
-                            //             )
-                            //         )}
-                            //     </div>
-                            // </div>
-                            <div className={styles.items}>
-                            <ListDetailCard list={list} imagewidth={185}/>
+                            <div className={styles}>
+                                {/* <button onClick={handleDelete} className={styles.headerbutton} data-id={list.id}>
+                                    {list.id === 8559472 ? <></> : <FontAwesomeIcon icon={faTrash} className={styles.headericon} />}
+                                </button> */}
+                                <div className={styles.items}>
+                                    <ListDetailCard list={list} imagewidth={185} />
+                                </div>
                             </div>
                         )
                     )}
