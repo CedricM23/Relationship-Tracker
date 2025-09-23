6e6420bfc79f2870b7e7cffa24bc38065061d002
cedricm23 <Cedricpbc@gmail.com>
2025-09-23 09:38:33 -0400

    Started working on lists and details

--- src/components/ListDetailCard/ListDetailCard.jsx
+++ src/components/ListDetailCard/ListDetailCard.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react"
 import ShowService from "../../services/ShowService";
 import styles from "../ListDetailCard/ListDetailCard.module.css"
+import { Link } from "react-router-dom";
+import placeholder from '../../images/no_poster.png'
 
-export default function ListDetailCard(list) {
+export default function ListDetailCard({list, imagewidth}) {
     const [listdetails, setListDetails] = useState([])
 
     useEffect(() => {
         //Fav list hardcoded
-        ShowService.getListDetailsById(list.list.id)
+        ShowService.getListDetailsById(list.id)
             .then((response) => {
                 setListDetails(response.data.results)
                 console.log('')
@@ -21,15 +23,24 @@ export default function ListDetailCard(list) {
 
     return (
         <div>
-            <div className={styles.listTtitle}>
-                {list.list.name}
+            <div className={styles.listTitle}>
+                {list.name}
             </div>
             {/* {JSON.stringify(listdetails)} */}
+            {/* list item */}
             <div className={styles.items}>
                 {listdetails.map(
                     (item) => (
+                        /* item container */
                         <div className={styles.itemContainer}>
-                            <div>{item.name ? item.name : item.title}</div>
+                            <Link to={`/watchlist/${item.id}/${item.media_type}`} className={styles.link}>
+                                <div>
+                                    {item.poster_path ?
+                                        <img src={`https://image.tmdb.org/t/p/w${imagewidth}/${item.poster_path}`} alt="poster" /> :
+                                        <img className={styles.placeholder} src={placeholder} alt="" />}
+                                </div>
+                                <div className={styles.cardTitle}>{item.name ? item.name : item.title}</div>
+                            </Link>
                         </div>
                     )
                 )}
