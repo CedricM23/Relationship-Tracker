0adc144b855a011e57be51bdf5d31c2bfb14f1d8
cedricm23 <Cedricpbc@gmail.com>
2025-10-09 05:56:17 -0400

    Added useEffect and service function for grabbing and storing show data

--- src/views/ShowsView/ShowsView.jsx
+++ src/views/ShowsView/ShowsView.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import WatchlistNav from "../../components/WatchlistNavBar/WatchlistNav";
+import ShowService from "../../services/ShowService";
+import MediaCard from "../../components/MediaCard/MediaCard";
 
 export default function ShowsView(){
+    const [shows, setShows] = useState([])
+
+    useEffect(() => {
+        ShowService.DiscoverShows()
+        .then((response) => {
+            setShows(response.data.results)
+        })
+    }, [])
+
     return (
         <>
         <WatchlistNav />
         <h1>Shows PlaceHolder</h1>
-        <h3>You'll be able to browse shows here</h3>
+        {/* {JSON.stringify(shows)} */}
+        {shows.map(
+            (show, index) => (
+                // poster_path not working
+                <MediaCard key={index} media={show} title={show.name} mediaType="tv"/>
+            )
+        )}
         </>
     )
 }
